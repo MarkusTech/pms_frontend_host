69c96a4dc04c596fb87d3c441879bdf60c0f78b6
MarkusTech <wm.recopelacion@gmail.com>
2024-12-01 16:18:48 +0800

    Update poSupplier.js

--- src/services/purchaseOrder/poSupplier.js
+++ src/services/purchaseOrder/poSupplier.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchSuppliers = async () => {
   try {
-    const response = await axios.get("/api/v2/suppliers");
+    const response = await axios.get(
+      "https://pms-backend-host.onrender.com/api/v2/suppliers"
+    );
     return response.data.data;
   } catch (error) {
     console.error("Error fetching suppliers:", error);
@@ -12,7 +14,10 @@ export const fetchSuppliers = async () => {
 
 export const createSupplier = async (supplierData) => {
   try {
-    const response = await axios.post("/api/v2/suppliers", supplierData);
+    const response = await axios.post(
+      "https://pms-backend-host.onrender.com/api/v2/suppliers",
+      supplierData
+    );
     return {
       success: true,
       data: response.data,
